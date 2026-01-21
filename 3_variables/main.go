16a5c469edd7373ac5f8cfcdffaf6e0cc82f9233
riaurko <riaurko@gmail.com>
2026-01-22 05:23:20 +0600

    feat: Recap upto variables

---
+++ 3_variables/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	// var name string = "Golang"
+
+	// fmt.Println(name)
+
+	// var name = "Golang"
+	// fmt.Println(name)
+
+	// var age int = 30
+	// fmt.Println(age)
+
+	//shorthand syntax
+	// name := "Golang"
+	// fmt.Println(name)
+
+	// var name string
+
+	// name = "Golang"
+	// fmt.Println(name)
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	price := 50.5
+	fmt.Println(price)
+}
